537ff37f2503636abcd61ecf417d6be13276882a
zproselkov <zproselkov@ozon.ru>
2022-03-12 18:13:31 +0300

    add vue.js fe

---
+++ re-front/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.count++
+        }
+    }
+}
+</script>
+
+<template>
+    <button @click="this.increment()">count is: {{ count }}</button>
+</template>
